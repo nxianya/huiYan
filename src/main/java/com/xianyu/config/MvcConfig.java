5a9063e8906c41b02663fffc83da3c4ce1285944
xianyu <2538420009@qq.com>
2023-06-03 23:01:31 +0800

    对拦截分类

---
+++ src/main/java/com/xianyu/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.xianyu.config;
+
+import com.xianyu.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "user/code",
+                        "user/login",
+                        "blog/hot",
+                        "shop/**",
+                        "shop-type/**",
+                        "voucher/**");
+    }
+}
