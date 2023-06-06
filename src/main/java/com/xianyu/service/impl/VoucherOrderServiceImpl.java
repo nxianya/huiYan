2d2d2cb59cd89938d9ebb537ab38741feeba11a6
xianyu <2538420009@qq.com>
2023-06-06 23:47:53 +0800

    解决单体模式下的超卖问题与一人一票需求

--- src/main/java/com/xianyu/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/xianyu/service/impl/VoucherOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.xianyu.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.xianyu.dto.Result;
 import com.xianyu.entity.SeckillVoucher;
 import com.xianyu.entity.VoucherOrder;
@@ -11,11 +12,15 @@ import com.xianyu.utils.CacheClient;
 import com.xianyu.utils.RedisIdWorker;
 import com.xianyu.utils.UserHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.time.LocalDateTime;
+
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.xianyu.utils.RedisConstants.*;
 
@@ -32,9 +37,13 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Autowired
     private RedisIdWorker redisIdWorker;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     @Transactional
     public Result seckillVoucher(Long voucherId) {
+
         SeckillVoucher seckillVoucher = seckillVoucherService.getById(voucherId);
         if (seckillVoucher.getBeginTime().isAfter(LocalDateTime.now())) {
             return Result.fail("秒杀尚未开始!");
@@ -45,19 +54,31 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (seckillVoucher.getStock()<1){
             return Result.fail("秒杀卷已抢完!");
         }
-        //扣减库存
-        boolean flag = seckillVoucherService.update().setSql("stock=stock-1").eq("voucher_id", voucherId).update();
-        if (!flag){
-            return Result.fail("库存不足!");
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()){
+            String jsonStr = stringRedisTemplate.opsForValue().get(LOGIN_USER_KEY + userId);
+            if ( StringUtils.isNotBlank(jsonStr)){
+                return Result.fail("用户已购买过此卷!");
+            }
+            int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+            if (count>0){
+                return Result.fail("用户已购买过此卷!");
+            }
+            //扣减库存
+            boolean flag = seckillVoucherService.update().setSql("stock=stock-1").eq("voucher_id", voucherId)
+                    .gt("stock",0).update();
+            if (!flag){
+                return Result.fail("库存不足!");
+            }
+            stringRedisTemplate.opsForValue().set(LOGIN_USER_KEY + userId,"yes",1L, TimeUnit.DAYS);
         }
         //创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
         long orderId = redisIdWorker.nextId(SECKILL_STOCK_KEY);
-        Long userId = UserHolder.getUser().getId();
         voucherOrder.setVoucherId(voucherId);
         voucherOrder.setUserId(userId);
         voucherOrder.setId(orderId);
         save(voucherOrder);
-        return Result.ok(voucherOrder);
+        return Result.ok(orderId);
     }
 }
