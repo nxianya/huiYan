6e401d1d2033a1309af230becd464b656e072e7d
xianyu <2538420009@qq.com>
2023-06-09 17:14:41 +0800

    暴露动态代理对象

--- src/main/java/com/xianyu/HyDianPingApplication.java
+++ src/main/java/com/xianyu/HyDianPingApplication.java
@@ -3,9 +3,11 @@ package com.xianyu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.xianyu.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 @EnableTransactionManagement
 @SpringBootApplication
 public class HyDianPingApplication {
