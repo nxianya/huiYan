e71e3a0928f33c5760695e7571f2ba457fbfaef5
xianyu <2538420009@qq.com>
2023-06-09 17:14:51 +0800

    提高setnx工具类的可用性

--- src/main/java/com/xianyu/utils/SimpleRedisLock.java
+++ src/main/java/com/xianyu/utils/SimpleRedisLock.java
@@ -1,13 +1,22 @@
 package com.xianyu.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
     private static final String LOCK_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     private String lock;
     private Object value;
 
@@ -38,14 +47,21 @@ public class SimpleRedisLock implements ILock {
         //考虑到拆装箱产生空指针的可能性
         return Boolean.TRUE.equals(flag);
     }
-
     @Override
     public void unLock() {
-        //获取线程标识
-        String id = stringRedisTemplate.opsForValue().get(LOCK_PREFIX + lock);
-        if (value.equals(id)) {
-            //释放锁
-            stringRedisTemplate.delete(LOCK_PREFIX + lock);
-        }
+        //调用lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(LOCK_PREFIX + lock),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unLock() {
+//        //获取线程标识
+//        String id = stringRedisTemplate.opsForValue().get(LOCK_PREFIX + lock);
+//        if (value.equals(id)) {
+//            //释放锁
+//            stringRedisTemplate.delete(LOCK_PREFIX + lock);
+//        }
+//    }
 }
